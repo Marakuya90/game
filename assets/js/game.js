b0d92048b1737315f6f648ef7f07f8b720ac4fe2
Marakuya90 <zhilzhil@mail.ru>
2022-10-16 00:24:31 +0300

    progress game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,32 +1,97 @@
 //Глобальные переменные
-let startButton = document.querySelector("#welcomeForm button")
+let startButton = document.querySelector('#welcomeForm button');
+let userName;
+let cells = document.querySelector('.field').children;
+const ROWS = 10;
+const COLS = 20;
+let player = {
+    row:0,
+    col:0
+};
+
+
+
 
 
 //События
+document.querySelector('#welcomeForm input').addEventListener('input', check);
+document.getElementById('welcomeForm').addEventListener('submit', start);
 
-document.querySelector("#welcomeForm input").addEventListener("input", check)
-document.getElementById("welcomeForm").addEventListener("submit", start)
 
 
-//Проверка ввода имени игрока
+
+//Проверка имени ввода игрока
 function check() {
-    //1 способ
-    /*if (this.value != "")
-        startButton.disabled = false
-    else
-        startButton.disabled = true*/
-
-    //2 способ
-    /*if (this.value)
-        startButton.disabled = false
-    else
-        startButton.disabled = true*/
-
-    //3 способ
-    startButton.disabled = !this.value
+    startButton.disabled = !this.value;
+    userName = this.value;
 }
 
+
 //Запуск игры
-function start(event){
-    event.preventDefault()
+function start(event) {
+    event.preventDefault();
+    //Скрываем форму входа
+    document.querySelector('.box-wrapper').classList.remove('active');
+    document.getElementById('hudUsername').innerText = userName;
+    fill();
+    window.addEventListener('keydown', move);
+
+}
+
+//Функция для заполнения игрового поля
+function fill() {
+//Помещаем игрока
+    cells[0].className = 'cell player';
+//Размещаем камни на игровом поле
+    let stonesCount = 0;
+    while (stonesCount < 10) {
+        let num = Math.floor(Math.random() * ROWS * COLS);
+        if (cells[num].classList.conteins = 'ground') {
+            cells[num].className = 'cell stone';
+            stonesCount++; 
+        }
+    }
+//Размещаем сердца на игровом поле
+    let heartCount = 0;
+    while (heartCount < 10) {
+        let num = Math.floor(Math.random() * 200);
+        if (cells[num].classList.conteins = 'ground') {
+            cells[num].className = 'cell heart';
+            heartCount++;}
+        }
+    }
+//Движение игрока
+function move(event) {
+    switch(event.keyCode){
+        case 37:left(); break;
+        case 38:up(); break;
+        case 39:right(); break;
+        case 40:down(); break;
+    }
+}
+
+function left(){
+    
+};
+
+function up(){
+
+};
+//движение вправо
+function right(){
+    if(player.col < COLS - 1) {
+        let currentCell = getCell(player.row, player.col);
+        let nextCell = getCell(player.row, player.col + 1);
+        currentCell.className = 'cell';
+        nextCell.className = 'cell player';
+        player.col++;
+    }
+};
+
+function down(){
+
+}
+
+function getCell(row, col){
+    return cells[row * COLS + col];
 }
